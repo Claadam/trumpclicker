3481443e764f3b2a001825b3ff5cfadea9a5d188
WalnutsAreCool <claadam@nuevaschool.org>
2016-10-21 09:29:19 -0700

    Classadoo autocommit

---
+++ script.js
@@ -0,0 +1,25 @@
+//var listvalue = ["trumpclicker"]
+var debt = $("#debt")
+ 
+
+var clickmoney = 1
+var money = 0
+var moneyowed = 0
+//document.getElementById('money').innerHTML = 'you have ' + money + " million dollars";
+
+document.getElementById("click").onclick = function click() {
+  money += clickmoney;
+  document.getElementById('money').innerHTML = 'you have ' + money + " million dollars";
+  var money2 = moneyowed+1
+  var moneyowed = money2*1.1
+  debt.html("you also have " + moneyowed + " million dollars of debt")
+  
+  
+//  addListItem(listvalue, money);
+//  console.log(listvalue)
+}
+
+function login(){
+console.log("lginpage")
+
+}
